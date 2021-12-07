b331c8c2ee97b0d1782927ea8f6848dc1a19d7b4
evc2bds <91164928+evc2bds@users.noreply.github.com>
2021-12-06 19:53:06 -0500

    Create API.py

--- API.py
+++ API.py
@@ -12,17 +12,16 @@ print()
 print("Here are the search results for", params["type"], "businesses in", params["location"], "with a rating of 4.0 or higher: ")
 print()
 
-#making a get request to the API
 request = requests.get(url, params = params, headers = headers)
 data = json.loads(request.text)
 
 #get list of businesses 
 businesses = data["businesses"]
  
-#print businesses' names, addresses, and phone number if they have a rating higher than 4.0
+#print businesses' names, phone number, and address if they have a rating higher than 4.0
 for business in businesses:
     if business["rating"] >= 4:
         print("Name:", business["name"])
+        print("Phone Number:", business["phone"])
         print("Address:", " ".join(business["location"]["display_address"]))
-        print("Phone:", business["phone"])
-        print("\n")
+        print()
